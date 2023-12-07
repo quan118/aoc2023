97416e8b7e1381f01a5611debb4a9c61d9a2987b
quan118 <quannt89@gmail.com>
2023-12-07 16:19:56 +0700

    Solution for day 4 - part 2

--- src/day4.rs
+++ src/day4.rs
@@ -1,9 +1,17 @@
+use std::cmp;
 use std::collections::HashSet;
 use std::fs;
 use std::io;
 
+#[derive(Clone)]
+struct Card {
+  quantity: u32,
+  matching_count: u32,
+}
+
 pub fn solve() {
-  println!("Part 1: {}", part1("inputs/day4/part1").unwrap())
+  println!("Part 1: {}", part1("inputs/day4/part1").unwrap());
+  println!("Part 2: {}", part2("inputs/day4/part1").unwrap());
 }
 
 fn part1(file: &str) -> io::Result<u32> {
@@ -11,16 +19,42 @@ fn part1(file: &str) -> io::Result<u32> {
   let mut total: u32 = 0;
   for line in input.lines() {
     let (winning_numbers, numbers_i_have) = get_numbers(line);
-    // println!("winning numbers: {:?}", winning_numbers);
-    // println!("numbers i have: {:?}", numbers_i_have);
     let my_winning_numbers = winning_numbers.intersection(&numbers_i_have).count() as u32;
     if my_winning_numbers > 0 {
-      total += (2 as u32).pow(my_winning_numbers-1);
-      
+      total += (2 as u32).pow(my_winning_numbers - 1);
     }
-    // println!("my winning numbers: {:?}", my_winning_numbers);
   }
 
+  Ok(total)
+}
+
+fn part2(file: &str) -> io::Result<u32> {
+  let input = fs::read_to_string(file)?;
+  let line_count = input.lines().count();
+  let mut cards = vec![
+    Card {
+      quantity: 1,
+      matching_count: 0
+    };
+    line_count
+  ];
+
+  for (idx, line) in input.lines().enumerate() {
+    let (winning_numbers, numbers_i_have) = get_numbers(line);
+    let my_winning_numbers = winning_numbers.intersection(&numbers_i_have).count() as u32;
+
+    cards[idx].matching_count = my_winning_numbers;
+
+    for idx2 in idx + 1..cmp::min(line_count, idx + cards[idx].matching_count as usize + 1) {
+      cards[idx2].quantity += cards[idx].quantity;
+    }
+  }
+
+  let total = cards
+    .iter()
+    .map(|a| a.quantity)
+    .reduce(|a, b| a + b)
+    .unwrap();
   Ok(total)
 }
 
