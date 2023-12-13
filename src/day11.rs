aa01cea455f637fdf87413a3af9da0aecf3659a2
quan118 <quannt89@gmail.com>
2023-12-13 11:06:20 +0700

    Solution for day 11 - part 2

--- src/day11.rs
+++ src/day11.rs
@@ -3,15 +3,74 @@ use std::io;
 
 pub fn solve() {
   println!("Part 1: {}", part1("inputs/day11/part1").unwrap());
+  println!("Part 2: {}", part2("inputs/day11/part1").unwrap());
 }
 
-fn part1(file: &str) -> io::Result<u32> {
+fn part1(file: &str) -> io::Result<u64> {
+  solveImpl(file, 2)
+}
+
+fn part2(file: &str) -> io::Result<u64> {
+  solveImpl(file, 1000000)
+}
+
+fn solveImpl(file: &str, expansion_times: u64) -> io::Result<u64> {
+  let (map, nrow, ncol) = read_map(file)?;
+
+  let (empty_rows, empty_cols) = get_empty_rows_and_cols(&map, nrow, ncol);
+
+  // find all galaxies
+  let galaxies = map
+    .iter()
+    .enumerate()
+    .filter(|(_, &value)| value == '#')
+    .map(|(idx, _)| idx)
+    .collect::<Vec<usize>>();
+
+  // find shortest paths between 2 galaxies
+  let mut total: u64 = 0;
+  for i in 0..galaxies.len() - 1 {
+    for j in i + 1..galaxies.len() {
+      let row1 = galaxies[i] / ncol;
+      let col1 = galaxies[i] % nrow;
+      let row2 = galaxies[j] / ncol;
+      let col2 = galaxies[j] % nrow;
+      let fromRow = row1.min(row2);
+      let toRow = row1.max(row2);
+      let fromCol = col1.min(col2);
+      let toCol = col1.max(col2);
+
+      for r in fromRow..toRow {
+        if empty_rows.contains(&(r as u32)) {
+          total += expansion_times;
+        } else {
+          total += 1;
+        }
+      }
+
+      for c in fromCol..toCol {
+        if empty_cols.contains(&(c as u32)) {
+          total += expansion_times;
+        } else {
+          total += 1;
+        }
+      }
+    }
+  }
+
+  Ok(total)
+}
+
+fn read_map(file: &str) -> io::Result<(Vec<char>, usize, usize)> {
   let input = fs::read_to_string(file)?;
   let nrow = input.lines().count();
   let ncol = input.lines().next().unwrap().len();
   let map = input.chars().filter(|c| *c != '\n').collect::<Vec<char>>();
 
-  // find empty row and empty col
+  Ok((map, nrow, ncol))
+}
+
+fn get_empty_rows_and_cols(map: &Vec<char>, nrow: usize, ncol: usize) -> (Vec<u32>, Vec<u32>) {
   let mut empty_rows: Vec<u32> = Vec::new();
   let mut empty_cols: Vec<u32> = Vec::new();
 
@@ -41,70 +100,5 @@ fn part1(file: &str) -> io::Result<u32> {
     }
   }
 
-  // find expanded rows and cols
-  let expanded_rows: Vec<u32> = empty_rows
-    .iter()
-    .enumerate()
-    .map(|(idx, &value)| idx as u32 + value + 1)
-    .collect();
-  let expanded_cols: Vec<u32> = empty_cols
-    .iter()
-    .enumerate()
-    .map(|(idx, &value)| idx as u32 + value + 1)
-    .collect();
-
-  // create expanded map
-  let expanded_map_nrow = nrow + empty_rows.len();
-  let expanded_map_ncol = ncol + empty_cols.len();
-  let mut expanded_map = vec![' '; expanded_map_nrow * expanded_map_ncol];
-
-  for row in expanded_rows {
-    for col in 0..expanded_map_ncol {
-      expanded_map[row as usize * expanded_map_ncol + col as usize] = '.';
-    }
-  }
-
-  for col in expanded_cols {
-    for row in 0..expanded_map_nrow {
-      expanded_map[row as usize * expanded_map_ncol + col as usize] = '.';
-    }
-  }
-
-  let mut j = 0;
-  for i in 0..expanded_map.len() {
-    if expanded_map[i] != '.' {
-      expanded_map[i] = map[j];
-      j += 1;
-    }
-  }
-
-  // print expanded map
-  // for row in 0..expanded_map_nrow {
-  //   for col in 0..expanded_map_ncol {
-  //     print!("{}", expanded_map[row as usize* expanded_map_ncol + col as usize]);
-  //   }
-  //   println!();
-  // }
-
-  // find all galaxies
-  let galaxies = expanded_map
-    .iter()
-    .enumerate()
-    .filter(|(_, &value)| value == '#')
-    .map(|(idx, _)| idx)
-    .collect::<Vec<usize>>();
-
-  // find shortest paths between 2 galaxies
-  let mut total: u32 = 0;
-  for i in 0..galaxies.len() - 1 {
-    for j in i + 1..galaxies.len() {
-      let row1 = galaxies[i] / expanded_map_ncol;
-      let col1 = galaxies[i] % expanded_map_ncol;
-      let row2 = galaxies[j] / expanded_map_ncol;
-      let col2 = galaxies[j] % expanded_map_ncol;
-      total += (row1 as i32 - row2 as i32).abs() as u32 + (col1 as i32 - col2 as i32).abs() as u32;
-    }
-  }
-
-  Ok(total)
+  (empty_rows, empty_cols)
 }
